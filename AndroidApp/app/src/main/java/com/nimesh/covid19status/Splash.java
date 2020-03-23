b148830dea6ffd209397b1ea9ba6aff1e2d2415a
nimeshkasun <nimesh.ekanayaka7@gmail.com>
2020-03-23 14:47:31 +0530

    App v2.0

--- AndroidApp/app/src/main/java/com/nimesh/covid19status/Splash.java
+++ AndroidApp/app/src/main/java/com/nimesh/covid19status/Splash.java
@@ -11,7 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
-    private TextView tv;
+    private TextView tv,tv2,tv3;
     private ImageView iv;
 
     @Override
@@ -20,15 +20,19 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         tv = (TextView) findViewById(R.id.splashAppIntro);
+        tv2 = (TextView) findViewById(R.id.splashAppIntro2);
+        tv3 = (TextView) findViewById(R.id.devBy);
         iv = (ImageView) findViewById(R.id.splashLogo);
         Animation splashAnim = AnimationUtils.loadAnimation(this,R.anim.splashtransition);
         tv.startAnimation(splashAnim);
+        tv2.startAnimation(splashAnim);
+        tv3.startAnimation(splashAnim);
         iv.startAnimation(splashAnim);
         final Intent in = new Intent(this, MainActivity.class);
         Thread timer = new Thread(){
             public void run(){
                 try {
-                    sleep(2000);
+                    sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }finally {
